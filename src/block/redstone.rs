48c714dc52fa8a1522931c311af1aab4935df83e
YummyOreo <bobgim20@gmail.com>
2023-04-02 16:45:59 -0500

    some more blocks

--- src/block/redstone.rs
+++ src/block/redstone.rs
@@ -13,10 +13,17 @@ pub enum RedstoneComponent {
     StickyPiston(RedstonePiston),
     Observer,
     Lamp,
+    TargetBlock(RedstoneTargetBlock),
+    NoteBlock,
+    Rail,
 }
 
 pub struct RedstoneDust {
     pub power_direction: Vec<Facing>,
+}
+
+impl RedstoneDust {
+    pub const UPDATE_DIRECTION: UpdateDirection = UpdateDirection::FromSource;
 }
 
 pub struct RedstoneTourch {
@@ -54,4 +61,24 @@ pub enum PistonPhase {
     Extenting,
     Extended,
     Retracting,
+}
+
+pub struct RedstoneTargetBlock {
+    /// -1 represents not on
+    pub delay_left: i16,
+    /// Depends what hits it
+    pub delay: i16,
+
+    pub caused_by_projectile: bool,
+}
+
+impl RedstoneTargetBlock {
+    pub const UPDATE_DIRECTION: UpdateDirection = UpdateDirection::AwaySource;
+}
+
+pub enum UpdateDirection {
+    /// From the source out
+    FromSource,
+    /// From last to source
+    AwaySource
 }
