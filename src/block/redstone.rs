37bb9f4098caef8d659e414ba504457fabe615de
YummyOreo <bobgim20@gmail.com>
2023-04-02 13:12:54 -0500

    some redstone stuff, the easy ones

--- src/block/redstone.rs
+++ src/block/redstone.rs
@@ -1,16 +1,57 @@
-use crate::block::Block;
+use super::Facing;
 
-pub trait Component {
-    fn get_strength(&self) -> i8;
-    fn set_strength(&mut self, strength: i8);
+pub enum RedstoneComponent {
+    Dust(RedstoneDust),
+    Block,
+    Tourch(RedstoneTourch),
+    Repeater(RedstoneRepeater),
+    Comparator(RedstoneRepeater),
+    Lever,
+    Button(RedstoneButton),
+    PressurePlate(RedstoneButton),
+    Piston(RedstonePiston),
+    StickyPiston(RedstonePiston),
+    Observer,
+    Lamp,
 }
 
-pub enum RedstoneComponent<'a> {
-    Dust(RedstoneDust<'a>)
+pub struct RedstoneDust {
+    pub power_direction: Vec<Facing>,
 }
 
-pub struct RedstoneDust<'a> {
-    pub position: (i32, i32),
-    pub strength: i8,
-    pub source: Option<&'a Block<'a>>,
+pub struct RedstoneTourch {
+    pub power_direction: Vec<Facing>,
+    pub is_burnt_out: bool,
+}
+
+pub struct RedstoneRepeater {
+    pub delay: i8,
+    /// -1 represents not on
+    pub delay_left: i8,
+
+    pub locked: bool,
+}
+
+pub struct RedstoneComparator {
+    pub subtract: bool,
+    pub signal_in: i8,
+    pub signal_left: i8,
+    pub signal_right: i8,
+}
+
+pub struct RedstoneButton {
+    pub delay: i16,
+    /// -1 represents not on
+    pub delay_left: i16,
+}
+
+pub struct RedstonePiston {
+    pub extent_phase: PistonPhase,
+}
+
+pub enum PistonPhase {
+    Retracted,
+    Extenting,
+    Extended,
+    Retracting,
 }
