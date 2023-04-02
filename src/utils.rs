d945be3c189292231480fb3d757c390e8873ae3b
YummyOreo <bobgim20@gmail.com>
2023-04-02 18:52:21 -0500

    adds from tick

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,13 @@
 use std::time::Duration;
 
+/// Assumes 20mspt
 pub fn to_ticks(time: Duration) -> u128 {
     let milis = time.as_millis();
     milis / 50
+}
+
+/// Assumes 20mspt
+pub fn from_ticks(ticks: u64) -> Duration {
+    let milis = ticks * 50;
+    Duration::from_millis(milis)
 }
