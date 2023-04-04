9e45887ce9b21a678c04676b1ee26815c025df70
YummyOreo <bobgim20@gmail.com>
2023-04-04 17:19:55 -0500

    does some workflows

--- phos-core/src/lib.rs
+++ phos-core/src/lib.rs
@@ -1,5 +1,9 @@
 #![warn(clippy::pedantic, clippy::nursery)]
-#![allow(clippy::use_self, clippy::missing_const_for_fn, clippy::must_use_candidate)]
+#![allow(
+    clippy::use_self,
+    clippy::missing_const_for_fn,
+    clippy::must_use_candidate
+)]
 
 pub mod block;
 pub mod utils;
