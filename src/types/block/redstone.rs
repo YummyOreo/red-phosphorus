0bd6c470c9eae20af9f6c34541f19bf158c84bd9
YummyOreo <bobgim20@gmail.com>
2023-04-19 19:27:11 -0500

    Simplifies Components (#21)
    * adds DelayState and removes unused fields
    
    * removes uneeded structs
    
    * adds some helper functions to `DelayState`
    
    * moves update direction
    
    * removes 1 space

--- src/types/block/redstone.rs
+++ src/types/block/redstone.rs
@@ -1,72 +1,122 @@
 use super::Facing;
 
+const DUST_UPDATE_DIRECTION: UpdateDirection = UpdateDirection::FromSource;
+const RAIL_UPDATE_DIRECTION: UpdateDirection = UpdateDirection::AwaySource;
+
+pub struct DelayState {
+    max_delay: i8,
+    /// If the component is not powered, then this should be set to -1
+    delay_left: i8,
+}
+
+impl DelayState {
+    pub fn new(max_delay: i8) -> Self {
+        Self {
+            max_delay,
+            delay_left: -1,
+        }
+    }
+
+    pub fn set_delay_left(&mut self, delay: i8) {
+        self.delay_left = delay;
+    }
+
+    pub fn get_delay(&self) -> i8 {
+        self.max_delay
+    }
+
+    pub fn get_delay_left(&self) -> Option<i8> {
+        if self.delay_left.is_negative() {
+            return None;
+        }
+        Some(self.delay_left)
+    }
+
+    /// Will set the delay to the max delay
+    pub fn power(&mut self) {
+        self.delay_left = self.max_delay;
+    }
+
+    /// Will set the delay to off
+    pub fn reset_delay(&mut self) {
+        self.delay_left = -1;
+    }
+
+    /// Will decrement the delay by 1 tick
+    /// Will return the ammount of delay left after operations
+    ///
+    /// If the block is not powered or the delay is not active. Then it will set it to the max
+    /// delay
+    pub fn decrement_delay(&mut self) -> i8 {
+        if self.delay_left.is_negative() || self.delay_left == 0 {
+            self.delay_left = self.max_delay + 1;
+        }
+        self.delay_left -= 1;
+        self.delay_left
+    }
+}
+
 pub enum Component {
-    Dust(Dust),
+    Dust {
+        power_direction: Vec<Facing>,
+    },
     Block,
-    Tourch(Tourch),
-    Repeater(Repeater),
-    Comparator(Repeater),
+    Tourch {
+        power_direction: Vec<Facing>,
+        burnt_out: bool,
+    },
+    Repeater {
+        delay: DelayState,
+        locked: bool,
+    },
+    Comparator {
+        subtract: bool,
+        /// Left, Middle (back), Right
+        signal_in: (i8, i8, i8),
+    },
     Lever,
     /// Redstone buttons
     /// Change the delay of the button based on the type of button
     ///
     /// Wooden: 30 ticks (15 redstone ticks)
     /// Stone: 20 ticks (10 redstone ticks)
-    Button(Button),
+    Button {
+        delay: DelayState,
+    },
     /// Iron and Gold pressure plates
     WeightedPressurePlate(Box<dyn WeightedPressurePlate>),
     /// Wooden pressure plates
     PressurePlate,
-    Piston(Piston),
+    Piston {
+        extent_phase: PistonPhase,
+    },
     PistonHead,
-    StickyPiston(Piston),
+    StickyPiston {
+        extent_phase: PistonPhase,
+    },
     SticyPistonHead,
     Observer,
     Lamp,
-    TargetBlock(TargetBlock),
+    TargetBlock {
+        delay: DelayState,
+    },
     NoteBlock,
     /// All rails that can be activated
-    Rail(Rail),
-    Lecturn(Lecturn),
+    Rail,
+    Lecturn,
     DoorUpper,
     DoorLower,
     Trapdoor,
 }
 
-pub struct Dust {
-    pub power_direction: Vec<Facing>,
-}
-
-impl Dust {
-    pub const UPDATE_DIRECTION: UpdateDirection = UpdateDirection::FromSource;
-}
-
-pub struct Tourch {
-    pub power_direction: Vec<Facing>,
-    pub is_burnt_out: bool,
-}
-
-pub struct Repeater {
-    pub delay: i8,
-    /// -1 represents not on
-    pub delay_left: i8,
-
-    pub locked: bool,
-}
-
-pub struct Comparator {
-    pub subtract: bool,
-    pub signal_in: i8,
-    pub signal_left: i8,
-    pub signal_right: i8,
-
-    pub output_strength: i8,
-}
-
-pub struct Button {
-    pub delay: i16,
-    /// -1 represents not on
-    pub delay_left: i16,
+impl Component {
+    pub fn get_update_direction(&self) -> Option<UpdateDirection> {
+        match self {
+            Self::Rail => Some(RAIL_UPDATE_DIRECTION),
+            Self::Dust { .. } => Some(DUST_UPDATE_DIRECTION),
+            _ => None,
+        }
+    }
 }
 
 pub trait WeightedPressurePlate {
@@ -76,41 +126,11 @@ pub trait WeightedPressurePlate {
     fn calc(&self) -> i8;
 }
 
-pub struct Piston {
-    pub extent_phase: PistonPhase,
-}
-
 pub enum PistonPhase {
     Retracted,
     Extenting,
     Extended,
     Retracting,
-}
-
-pub struct TargetBlock {
-    /// -1 represents not on
-    pub delay_left: i16,
-    /// Depends what hits it
-    pub delay: i16,
-
-    pub caused_by_projectile: bool,
-
-    pub output_strength: i8,
-}
-
-pub struct Rail;
-
-impl Rail {
-    pub const UPDATE_DIRECTION: UpdateDirection = UpdateDirection::AwaySource;
-}
-
-pub struct Lecturn {
-    /// -1 represents not on
-    pub delay_left: i16,
-    /// Depends what hits it
-    pub delay: i16,
-
-    pub output_strength: i8,
 }
 
 pub enum UpdateDirection {
