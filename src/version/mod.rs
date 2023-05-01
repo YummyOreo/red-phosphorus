33d58b9ae1109c50f8304504e394aa6e154cb18d
YummyOreo <bobgim20@gmail.com>
2023-05-01 15:07:28 -0500

    Possibly fixes ci failing

--- src/version/mod.rs
+++ src/version/mod.rs
@@ -9,6 +9,7 @@ impl Default for Version {
         Self::Verson1_19
     }
 
+    #[not(default)]
     fn default() -> Self {
         panic!("You must have at least one version feature enabled")
     }
@@ -18,7 +19,7 @@ impl ToString for Version {
     fn to_string(&self) -> String {
         match self {
             #[cfg(feature = "1_19")]
-            Self::Verson1_19 => Some(String::from("1.19.*")),
+            Self::Verson1_19 => String::from("1.19.*"),
             _ => panic!("You must have at least one version feature enabled"),
         }
     }
