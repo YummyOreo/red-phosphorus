5752533ff6c601f3cc469a44d153377df9086319
YummyOreo <bobgim20@gmail.com>
2023-05-06 08:58:31 -0500

    feat(compiler): start of compiler
    adds the entry for the compiler
    
    pr: #28

--- src/types/contraption.rs
+++ src/types/contraption.rs
@@ -1,16 +1,25 @@
-use super::block::Block;
+use super::{block::Block, compiler::Graph};
 use crate::version::Version;
 
+pub type Position = (u16, u16, u16);
+
 pub trait World<'a> {
-    fn get_block(&self, pos: (i8, i8, i8)) -> &'a dyn Block<'a>;
-    fn get_block_mut(&mut self, pos: (i8, i8, i8)) -> &'a mut dyn Block<'a>;
+    fn get_block(&self, pos: Position) -> &'a dyn Block<'a>;
+    fn get_block_mut(&mut self, pos: Position) -> &'a mut dyn Block<'a>;
+
+    fn get_has_updated(&self) -> bool;
+    fn get_has_state_updated(&self) -> bool;
+
+    fn bounds(&self) -> (Position, Position);
 }
+
 /// Modling the blocks supplied for the contraption
 /// Warning: You should not supplie the whole world, this will be slow. You should supplie each
 /// contraption. This allows for you to use multi-threading
 pub struct Contraption<'a, T: World<'a>> {
     world: &'a mut T,
     verson: Version,
+    last_graph: Option<Graph>,
 }
 
 impl<'a, T: World<'a>> Contraption<'a, T> {
@@ -18,6 +27,7 @@ impl<'a, T: World<'a>> Contraption<'a, T> {
         Self {
             world,
             verson: Version::default(),
+            last_graph: None,
         }
     }
 
@@ -37,6 +47,14 @@ impl<'a, T: World<'a>> Contraption<'a, T> {
     /// Set the MC version for the contraption
     pub fn set_version(&mut self, version: Version) {
         self.verson = version;
+    }
+
+    pub fn get_graph(&self) -> Option<Graph> {
+        self.last_graph.clone()
+    }
+
+    pub fn has_graph(&self) -> bool {
+        self.last_graph.is_some()
     }
 
     #[allow(clippy::missing_panics_doc)]
--- src/utils/components.rs
+++ src/utils/components.rs
@@ -1,18 +1,20 @@
+use crate::types::PowerLevel;
+
 /// Input # of entities. If none, then just supplie 0
-pub fn calc_gold_plate(entities: i16) -> i8 {
-    entities.clamp(0, 15) as i8
+pub fn calc_gold_plate(entities: i16) -> PowerLevel {
+    entities.clamp(0, 15) as PowerLevel
 }
 
 #[allow(clippy::cast_possible_truncation)]
 /// Input # of entities. If none, then just supplie 0
-pub fn calc_iron_plate(entities: i16) -> i8 {
+pub fn calc_iron_plate(entities: i16) -> PowerLevel {
     // Each "range" is (powerlevel) - 10 + 1 .. (powerlevel * 10)
 
     let remainder = entities % 10;
     // Checks if divisable by 10
     if remainder == 0 {
         // If it is, then we can just return what was in the 10's place
-        return (entities / 10).clamp(0, 15) as i8;
+        return (entities / 10).clamp(0, 15) as PowerLevel;
     }
 
     // if not divisable by 10
@@ -20,7 +22,7 @@ pub fn calc_iron_plate(entities: i16) -> i8 {
     // then remove the one's place ie. 21 - 1 = 20
     // then divide by 10           ie. 20 / 10 = 2
     // the product is then clamped to 15, then converted to a i8
-    (((entities + 10) - (remainder)) / 10).clamp(0, 15) as i8
+    (((entities + 10) - (remainder)) / 10).clamp(0, 15) as PowerLevel
 }
 
 #[cfg(test)]
