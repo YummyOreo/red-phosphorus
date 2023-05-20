4715c9f315a99ef087bf47898641f1c1a7d99b3d
YummyOreo <bobgim20@gmail.com>
2023-05-20 14:54:36 -0500

    refactor: moves out the first node creation
    This is done to allow for testing.

--- src/compiler/full.rs
+++ src/compiler/full.rs
@@ -8,23 +8,36 @@ use crate::{
 
 pub mod single_thread {
     use super::*;
-    use crate::compiler::graph::match_block;
+    use crate::{compiler::graph::match_block, types::compiler::Node};
 
-    pub fn full_compile<'a, W: World<'a>>(world: &'a mut W) -> Graph {
+    fn make_first_node<'a, W: World<'a>>(world: &'a W) -> Option<Node> {
+        let blocks = world.get_blocks((-1, 0, 0));
+        let mut node = None;
+        for pos in blocks {
+            let block = world.get_block(pos);
+            if let Some(n) = match_block(block, pos) {
+                node = Some(n);
+                break;
+            }
+        }
+        node
+    }
+
+    pub fn full_compile<'a, W: World<'a>>(world: &'a mut W) -> Option<Graph> {
         let bounds = world.bounds();
         let _size = calc_bounds(bounds);
 
         let mut state = State::new(bounds.0);
 
-        let blocks = world.get_blocks((-1, 0, 0));
-        for pos in blocks {
-            let block = world.get_block(pos);
-            if let Some(node) = match_block(block, pos) {
-                state.graph = Some(Graph::new(node));
-                break;
+        match make_first_node(world) {
+            Some(node) => state.graph = Some(Graph::new(node)),
+            None => {
+                // All blocks are air, thus no need for a graph
+                return None;
             }
         }
 
+        let _blocks = world.get_blocks(state.graph.expect("Should be there").root.borrow().pos);
         todo!()
     }
 }
