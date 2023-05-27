03422809a9e5f86b294e2aa85ce5cf9c2419057b
YummyOreo <bobgim20@gmail.com>
2023-05-27 10:16:29 -0500

    feat: adds power tracking to more things

--- src/compiler/graph.rs
+++ src/compiler/graph.rs
@@ -8,7 +8,11 @@ use crate::types::{
 pub fn match_block(block: &Block, pos: Position) -> Option<Node> {
     let is_solid = block.is_solid();
     match block.get_kind() {
-        Kind::Block if is_solid => Some(Node::new(NodeKind::Block, pos)),
+        Kind::Block if is_solid => Some(Node::new_with_power(
+            NodeKind::Block,
+            pos,
+            block.get_power(),
+        )),
         Kind::Block => None,
         Kind::Component(component) => Some(match_component(block, component, pos)),
     }
@@ -18,12 +22,13 @@ pub fn match_component(block: &Block, component: &Component, pos: Position) -> N
     match component {
         Component::Block => Node::new_with_power(NodeKind::PowerSource, pos, 15),
         Component::Dust => Node::new_with_power(NodeKind::Dust, pos, block.get_power()),
-        Component::Repeater { delay, locked } => Node::new(
+        Component::Repeater { delay, locked } => Node::new_with_power(
             NodeKind::Repeater {
                 delay: *delay,
                 locked: *locked,
             },
             pos,
+            block.get_power(),
         ),
         Component::Lamp => Node::new_with_power(NodeKind::Lamp, pos, block.get_power()),
         _ => {
