56f0fb68ae718a0b9c9eb7326cd6877aef511c33
YummyOreo <bobgim20@gmail.com>
2023-06-07 08:49:03 -0500

    feat: adds getting the facing blocks

--- src/compiler/graph/link_nodes.rs
+++ src/compiler/graph/link_nodes.rs
@@ -1,8 +1,9 @@
 use petgraph::stable_graph::NodeIndex;
 
 use crate::types::{
+    block::{Block, Facing},
     compiler::{Graph, Link, Node},
-    contraption::World,
+    contraption::{Position, World},
 };
 
 fn get_connections<'a, W: World<'a>>(
@@ -10,5 +11,29 @@ fn get_connections<'a, W: World<'a>>(
     world: &'a W,
     graph: &'a Graph,
 ) -> Vec<(NodeIndex, Link)> {
+    let block = world
+        .get_block(node.pos)
+        .expect("all nodes should correspond to a block");
     todo!()
+}
+
+fn get_facing_blocks<'a, W: World<'a>>(
+    pos: Position,
+    facing: Vec<Facing>,
+    world: &'a W,
+) -> Vec<Option<&Block>> {
+    let mut blocks = vec![];
+    for side in facing {
+        let mut new_pos = pos;
+        match side {
+            Facing::NegativeZ => new_pos.2 -= 1,
+            Facing::PositiveZ => new_pos.2 += 1,
+            Facing::NegativeX => new_pos.0 -= 1,
+            Facing::PositiveX => new_pos.0 += 1,
+            Facing::NegativeY => new_pos.1 -= 1,
+            Facing::PositiveY => new_pos.1 += 1,
+        }
+        blocks.push(world.get_block(new_pos));
+    }
+    blocks
 }
