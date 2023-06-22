4a9fae811046d8209475a005ba540e421fd44088
YummyOreo <bobgim20@gmail.com>
2023-06-22 08:55:13 -0500

    feat: creat_graph fn

--- src/compiler/graph/mod.rs
+++ src/compiler/graph/mod.rs
@@ -1,4 +1,12 @@
 mod link_nodes;
 mod make_nodes;
 
-pub mod single_threaded {}
+pub mod single_threaded {
+    use crate::types::{compiler::Graph, contraption::World};
+    use super::{*, link_nodes::link_nodes};
+
+    pub fn create_graph<'a, W: World<'a>>(world: &'a W, cache: &mut mini_moka::sync::Cache<u64, crate::types::compiler::Node>) -> Graph {
+        let graph = make_nodes::make_nodes(world, cache);
+        link_nodes(graph, world)
+    }
+}
