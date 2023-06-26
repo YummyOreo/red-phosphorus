1cf17e8a76dd58e5c40ae61ebb97bda71591fdd7
YummyOreo <bobgim20@gmail.com>
2023-06-26 11:37:27 -0500

    refactor: use Sources instead of Vec^<NodeInedx^>

--- src/compiler/optimization/links.rs
+++ src/compiler/optimization/links.rs
@@ -1,8 +1,8 @@
 use petgraph::stable_graph::{EdgeIndex, NodeIndex};
 
-use crate::types::compiler::Graph;
+use crate::types::compiler::{Graph, Sources};
 
-pub fn remove_unused_links(graph: Graph, power_sources: Vec<NodeIndex>) -> Graph {
+pub fn remove_unused_links(graph: Graph, power_sources: Sources) -> Graph {
     let removed_edges: Vec<EdgeIndex> = vec![];
     todo!()
 }
