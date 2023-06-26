6a374dc6697246d243991ee99c0984ed8abd295c
YummyOreo <bobgim20@gmail.com>
2023-06-26 17:54:49 -0500

    feat: start with plans

--- src/compiler/optimization/links.rs
+++ src/compiler/optimization/links.rs
@@ -2,7 +2,11 @@ use petgraph::stable_graph::{EdgeIndex, NodeIndex};
 
 use crate::types::compiler::{Graph, Sources};
 
+// Go through each node, and run dijkstra's algorithm to see if it connects to a source
+// if id does not, then that node (and the edges going out of it) is removed
 pub fn remove_unused_links(graph: Graph, power_sources: Sources) -> Graph {
-    let removed_edges: Vec<EdgeIndex> = vec![];
+    for node_index in graph.node_indices() {
+        // do stuff
+    }
     todo!()
 }
