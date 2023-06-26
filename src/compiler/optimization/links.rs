ba7326079a0ba2c513a38c81602ecb39df55e4dc
YummyOreo <bobgim20@gmail.com>
2023-06-26 08:50:20 -0500

    feat: boilerplate for removing unused links

---
+++ src/compiler/optimization/links.rs
@@ -0,0 +1,8 @@
+use petgraph::stable_graph::{EdgeIndex, NodeIndex};
+
+use crate::types::compiler::Graph;
+
+pub fn remove_unused_links(graph: Graph, power_sources: Vec<NodeIndex>) -> Graph {
+    let removed_edges: Vec<EdgeIndex> = vec![];
+    todo!()
+}
