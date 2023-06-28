0bfde673ea04ccb52739a9901fc9cea5237f4117
YummyOreo <bobgim20@gmail.com>
2023-06-28 08:14:32 -0500

    feat: finish remove unused likns

--- src/compiler/optimization/links.rs
+++ src/compiler/optimization/links.rs
@@ -8,9 +8,19 @@ use crate::types::compiler::{Graph, Sources};
 // Or run a algo that will find every node that can be reached, log that, and remove any that can't
 // be reached via a power source
 // Then remove the edges that are going out of that node and into that node
-pub fn remove_unused_links(graph: Graph, power_sources: Sources) -> Graph {
-    let mut keep_nodes = get_reachable_nodes(&graph, power_sources);
-    todo!()
+pub fn remove_unused_links(mut graph: Graph, power_sources: Sources) -> Graph {
+    let keep_nodes = get_reachable_nodes(&graph, power_sources);
+    let mut remove_nodes = vec![];
+    for nx in graph.node_indices() {
+        if !keep_nodes.contains(&nx) {
+            remove_nodes.push(nx);
+        }
+    }
+
+    for nx in remove_nodes {
+        graph.remove_node(nx);
+    }
+    graph
 }
 
 pub fn get_reachable_nodes(graph: &Graph, power_sources: Sources) -> Vec<NodeIndex> {
