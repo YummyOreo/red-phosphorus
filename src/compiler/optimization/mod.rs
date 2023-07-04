c4c63da1da43af59dcc6b57c29903d59156114ed
YummyOreo <bobgim20@gmail.com>
2023-07-04 09:21:52 -0500

    feat: gets ready for combining redstone dust

--- src/compiler/optimization/mod.rs
+++ src/compiler/optimization/mod.rs
@@ -1,6 +1,7 @@
 use crate::types::compiler::{Graph, Sources};
 
 mod links;
+mod combine;
 
 pub fn optimize(graph: Graph, sources: Sources) -> Graph {
     links::remove_unused_links(graph, sources)
