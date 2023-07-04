2cf87feee1a383bd1386e5eb6f3030c8b4be5a33
YummyOreo <bobgim20@gmail.com>
2023-07-04 10:14:54 -0500

    feat: starts going through the graph

--- src/compiler/optimization/combine.rs
+++ src/compiler/optimization/combine.rs
@@ -1,4 +1,6 @@
-use crate::types::compiler::Graph;
+use petgraph::{stable_graph::NodeIndex, visit::Dfs};
+
+use crate::types::compiler::{Graph, Sources};
 
 // Go through and find dust that are connected and that are more than 2 long
 // Then add them to a list of list:
@@ -7,6 +9,10 @@ use crate::types::compiler::Graph;
 // ]
 // then combine these into a link between 2 non dust nodes
 // If can't make a link, then just link it to a dust at the end
-pub fn combine_redstone_dust(graph: Graph) -> Graph {
-    todo!();
+pub fn combine_redstone_dust(mut graph: Graph, sources: Sources) -> Graph {
+    let mut dust: Vec<Vec<NodeIndex>> = vec![];
+    for source in sources {
+        let dfs = Dfs::new(&graph, source);
+    }
+    todo!()
 }
--- src/compiler/optimization/mod.rs
+++ src/compiler/optimization/mod.rs
@@ -1,8 +1,8 @@
 use crate::types::compiler::{Graph, Sources};
 
-mod links;
 mod combine;
+mod links;
 
 pub fn optimize(graph: Graph, sources: Sources) -> Graph {
-    combine::combine_redstone_dust(links::remove_unused_links(graph, sources))
+    combine::combine_redstone_dust(links::remove_unused_links(graph, sources), sources)
 }
