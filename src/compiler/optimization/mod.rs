e56dda6008c6d98c6386a09d231451bd1db23dc5
YummyOreo <bobgim20@gmail.com>
2023-07-05 13:46:50 -0500

    feat: postpone dust combination to later

--- src/compiler/optimization/mod.rs
+++ src/compiler/optimization/mod.rs
@@ -1,8 +1,7 @@
 use crate::types::compiler::{Graph, Sources};
 
-mod combine;
 mod links;
 
 pub fn optimize(graph: Graph, sources: Sources) -> Graph {
-    combine::combine_redstone_dust(links::remove_unused_links(graph, sources.clone()), sources)
+    links::remove_unused_links(graph, sources)
 }
