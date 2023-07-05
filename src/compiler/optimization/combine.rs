d80de9f926755f68269572591fd48a2bce71b18d
YummyOreo <bobgim20@gmail.com>
2023-07-05 09:21:31 -0500

    feat: get all dusts in the graph

--- src/compiler/optimization/combine.rs
+++ src/compiler/optimization/combine.rs
@@ -1,6 +1,6 @@
 use petgraph::{stable_graph::NodeIndex, visit::Dfs};
 
-use crate::types::compiler::{Graph, Sources};
+use crate::types::compiler::{Graph, Sources, NodeKind};
 
 // Go through and find dust that are connected and that are more than 2 long
 // Then add them to a list of list:
@@ -12,7 +12,14 @@ use crate::types::compiler::{Graph, Sources};
 pub fn combine_redstone_dust(mut graph: Graph, sources: Sources) -> Graph {
     let mut dust: Vec<Vec<NodeIndex>> = vec![];
     for source in sources {
-        let dfs = Dfs::new(&graph, source);
+        let mut dfs = Dfs::new(&graph, source);
+        let mut dusts = vec![];
+        while let Some(nx) = dfs.next(&graph) {
+            let node = graph.node_weight(nx).unwrap();
+            if node.kind == NodeKind::Dust {
+                dusts.push(nx);
+            }
+        }
     }
     todo!()
 }
